5515e04d7cbe2d391509def3d122aded97501d84
jannyekap <jannyeka98@gmail.com>
2023-12-20 07:38:16 +0700

    minor update

--- js/script.js
+++ js/script.js
@@ -8,3 +8,32 @@ function toggleMenu() {
     const navItems = document.querySelector('.nav-items');
     navItems.classList.toggle('show');
 }
+
+function getValueFromForm(currTime, name, birthDate, gender, messages) {
+    document.getElementById('currTime').innerHTML = currTime;
+    document.getElementById('getFullName').innerHTML = name;
+    document.getElementById('getBirthDate').innerHTML = birthDate;
+    document.getElementById('getGender').innerHTML = gender;
+    document.getElementById('getMessages').innerHTML = messages;
+}
+
+function validateForm() { 
+    const currTime = new Date();
+    const name = document.forms['message-form']['fullName'].value;
+    const birthDate = document.forms['message-form']['birthDate'].value;
+    const gender = document.forms['message-form']['gender'].value;
+    const messages = document.forms['message-form']['messages'].value;
+
+    //Validasi input
+    if (name == "" || birthDate == "" || gender == "" || messages == "") {
+        alert("Tidak boleh ada data yang kosong");
+        return false;
+    }
+
+    getValueFromForm(currTime, name, birthDate, gender, messages);
+    return false;
+
+}
+
+var currYear = new Date().getFullYear();
+document.getElementById('currYear').innerText = currYear;
